131fc9b23ed1781ed8d93f1af8c24946e3900a76
nbsamurai <subbu@live.in>
2026-01-17 16:07:04 +0530

    Added get notes and delete note feature

--- convex/notes.ts
+++ convex/notes.ts
@@ -1,6 +1,17 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const getUserNotes = query({
+    args: {},
+    handler: async (ctx) => {
+        const userId = await getAuthUserId(ctx);
+        if (!userId) {
+            return [];
+        }
+        return await ctx.db.query("notes").withIndex("by_user_id", (q) => q.eq("userId", userId)).order("desc").collect();
+    },
+});
 
 export const createNote = mutation({
     args: {
@@ -18,5 +29,27 @@ export const createNote = mutation({
             body: args.body,
             userId: userId,
         });
+    },
+});
+
+export const deleteNote = mutation({
+    args: {
+        noteId: v.id("notes"),
+    },
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx);
+        if (!userId) {
+            throw new Error("User must be authenticated to delete a note");
+        }
+        
+        const note = await ctx.db.get(args.noteId);
+        if (!note) {
+            throw new Error("Note not found");
+        }
+        if (note.userId !== userId) {
+            throw new Error("User does not have permission to delete this note");
+        }
+        await ctx.db.delete(args.noteId);
+        return true;
     },
 });
